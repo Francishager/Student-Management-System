bb6c3a488aa1ea2e7947f3dc91a7ae6fba0f3651
Francishager <87711592+Francishager@users.noreply.github.com>
2025-03-21 03:59:14 -0700

    Update db.php

---
+++ config/db.php
@@ -0,0 +1,8 @@
+
+    <?php
+        define('db_host', 'sql10.freesqldatabase.com');
+        define('db_user', 'sql10768842');
+        define('db_pass', '2NH67i1cKf');
+        define('db_name', 'sql10768842');
+    ?>
+    
